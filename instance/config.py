d6736c76dabf46bd6c69c13359cdf4aa1f6f3cfa
Danny Tamez <zematynnad@gmail.com>
2017-04-06 15:39:54 -0500

    Move secret values out of config.py and pull them from environment vars. Add config to sourc control.

---
+++ instance/config.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+DEBUG = os.getenv('DEBUG', False)
+DEBUG_TB_INTERCEPT_REDIRECTS = os.getenv('DEBUG_TB_INTERCEPT_REDIRECTS', False)
+SECRET_KEY = os.getenv('SECRET_KEY', 'Not very secret')  # '78asfoijja*sd0^0qwer098nq535)N&)N)wrjq0w450'
+
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQL_ALCHEMY_ECHO = os.getenv('SQL_ALCHEMY_ECHO', False)
+SQLALCHEMY_RECORD_QUERIES = DEBUG
+SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
+
+MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = os.getenv('MAIL_USERNAME')  #
+MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')  # 'jmbwkcdfcarnifxl'
+MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', 'noreply@perks.co')
+
+ADMINS = [os.getenv('ADMINS')]
