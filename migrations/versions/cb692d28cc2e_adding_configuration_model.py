af3ff3224e786a83bbae399af6e43e8cfd86801a
Eric Gonzalez <eric.gonz.gomez@gmail.com>
2018-01-07 18:09:31 -0600

    Adding configuration migration script.

---
+++ migrations/versions/cb692d28cc2e_adding_configuration_model.py
@@ -0,0 +1,24 @@
+"""Adding configuration model
+
+Revision ID: cb692d28cc2e
+Revises: 6f3ea3429c0b
+Create Date: 2018-01-07 17:58:13.953004
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cb692d28cc2e'
+down_revision = '6f3ea3429c0b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
